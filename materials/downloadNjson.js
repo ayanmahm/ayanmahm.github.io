222324a6f80d98b28fe410d91c2b7035ec593b31
ayan <ayan04@live.co.uk>
2023-10-01 21:12:38 +0100

    new json file

--- materials/downloadNjson.js
+++ materials/downloadNjson.js
@@ -5,10 +5,10 @@ function downloadFile() {
     var a = document.createElement("a");
     
     // Set the href attribute to the file you want to download
-    a.href = "280923notionEnhancer.json";
+    a.href = "011023notionEnhancer.json";
     
     // Set the download attribute with the desired file name
-    a.download = "280923notionEnhancer.json";
+    a.download = "011023notionEnhancer.json";
     
     // Trigger a click event on the anchor element to start the download
     a.click();
